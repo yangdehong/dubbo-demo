f8cf9fdd543fdf356c96e88dd8a2f743766c12d6
yangdehong <405104787@qq.com>
2020-11-09 16:31:50 +0800

    fastjson用来telnet

--- dubbo-provider/src/main/java/com/ydh/redsheep/dubboprovider/service/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/ydh/redsheep/dubboprovider/service/DemoServiceImpl.java
@@ -4,8 +4,6 @@ import com.ydh.redsheep.dubboapi.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Service;
 
-import java.time.LocalDateTime;
-
 /**
 *
 * @author : yangdehong
@@ -16,13 +14,13 @@ import java.time.LocalDateTime;
 public class DemoServiceImpl implements DemoService {
     @Override
     public String sayHello(String name) {
-        log.info("时间={}", LocalDateTime.now());
-        try {
-            Thread.sleep(60*1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        log.info("时间={}", LocalDateTime.now());
+//        log.info("时间={}", LocalDateTime.now());
+//        try {
+//            Thread.sleep(60*1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info("时间={}", LocalDateTime.now());
         return "Hello " + name;
     }
 }
