eafe25a8e87238e628a9acc08c2167c3a2d95d7d
yangdehong <405104787@qq.com>
2021-01-03 14:55:33 +0800

    filter loadbalance

--- self-filter/src/main/java/com/ydh/redsheep/selffilter/SelfFilterApplication.java
+++ self-filter/src/main/java/com/ydh/redsheep/selffilter/SelfFilterApplication.java
@@ -1,13 +1,13 @@
-package com.ydh.redsheep.selffilter;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SelfFilterApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SelfFilterApplication.class, args);
-    }
-
-}
+//package com.ydh.redsheep.selffilter;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SelfFilterApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SelfFilterApplication.class, args);
+//    }
+//
+//}
--- self-loadbalance/src/main/java/com/ydh/redsheep/selfloadbalance/SelfLoadbalanceApplication.java
+++ self-loadbalance/src/main/java/com/ydh/redsheep/selfloadbalance/SelfLoadbalanceApplication.java
@@ -1,13 +1,13 @@
-package com.ydh.redsheep.selfloadbalance;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SelfLoadbalanceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SelfLoadbalanceApplication.class, args);
-    }
-
-}
+//package com.ydh.redsheep.selfloadbalance;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SelfLoadbalanceApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SelfLoadbalanceApplication.class, args);
+//    }
+//
+//}
