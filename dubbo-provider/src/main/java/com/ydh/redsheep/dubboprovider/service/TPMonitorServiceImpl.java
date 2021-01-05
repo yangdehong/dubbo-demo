800055ce1434acf932729ed6256d49954f02e469
yangdehong <405104787@qq.com>
2021-01-05 12:00:34 +0800

    tp99 tp90

--- dubbo-provider/src/main/java/com/ydh/redsheep/dubboprovider/service/TPMonitorServiceImpl.java
+++ dubbo-provider/src/main/java/com/ydh/redsheep/dubboprovider/service/TPMonitorServiceImpl.java
@@ -15,42 +15,42 @@ public class TPMonitorServiceImpl implements TPMonitorService {
 
     static {
         System.out.println("========TP90 TP99 ");
-        new Thread(() -> {
-            while (true) {
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    ConcurrentMap<String, List<Long>> map = TPMonitorFilter.map;
-                    map.keySet().forEach(key -> {
-                        List<Long> list = map.get(key);
-                        if (CollectionUtils.isEmpty(list)) {
-                            return;
-                        }
-                        Collections.sort(list);
-                        int size = list.size();
-                        // TP90：满足百分之九十的网络请求所需的最低耗时
-                        // TP99：满足百分之九十九的网络请求所需的最低耗时
-                        boolean flag99 = true;
-                        boolean flag90 = true;
-                        for (int i = size-1; i >= 0; i--) {
-                            if (flag99 && (i+1)/size<0.99) {
-                                System.out.println(key+"的TP99="+list.get(i));
-                                flag99 = false;
-                            }
-                            if (flag90 && (i+1)/size<0.90) {
-                                System.out.println(key+"的TP90="+list.get(i));
-                                flag90 = false;
-                            }
-                        }
-                    });
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+//        new Thread(() -> {
+//            while (true) {
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    ConcurrentMap<String, List<Long>> map = TPMonitorFilter.map;
+//                    map.keySet().forEach(key -> {
+//                        List<Long> list = map.get(key);
+//                        if (CollectionUtils.isEmpty(list)) {
+//                            return;
+//                        }
+//                        Collections.sort(list);
+//                        int size = list.size();
+//                        // TP90：满足百分之九十的网络请求所需的最低耗时
+//                        // TP99：满足百分之九十九的网络请求所需的最低耗时
+//                        boolean flag99 = true;
+//                        boolean flag90 = true;
+//                        for (int i = size-1; i >= 0; i--) {
+//                            if (flag99 && (i+1)/size<0.99) {
+//                                System.out.println(key+"的TP99="+list.get(i));
+//                                flag99 = false;
+//                            }
+//                            if (flag90 && (i+1)/size<0.90) {
+//                                System.out.println(key+"的TP90="+list.get(i));
+//                                flag90 = false;
+//                            }
+//                        }
+//                    });
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
     }
 
     @Override
